14126c6a217fc292f9637cef6a528cc7350a3d8d
Karel van Klink <karel.vanklink@geant.org>
2023-04-17 11:25:52 +0200

    Rework minor details

--- test/test_create_venv.py
+++ test/test_create_venv.py
@@ -9,7 +9,7 @@ TEST_CONFIG = {
 
 
 def test_create_venv_and_run_playbook():
-    with TemporaryDirectory() as venv_path:
+    with TemporaryDirectory(prefix='lso-venv') as venv_path:
         # Instantiate a new venv
         subprocess.check_call(['python3', '-m', 'venv', venv_path])
 
@@ -23,9 +23,13 @@ def test_create_venv_and_run_playbook():
 
         #  Run Ansible dummy playbook
         ansible_playbook_path = os.path.join(venv_path, 'bin', 'ansible-playbook')
-        playbook_run = subprocess.check_output(
-            [ansible_playbook_path, '-i', 'localhost,', 'playbook.yml', '--connection=local']).decode()
+        playbook_run = subprocess.run(
+            [ansible_playbook_path, '-i', 'localhost,', 'playbook.yml', '--connection=local'],
+            capture_output=True
+        )
 
-        assert ' '.join(playbook_run.split()).endswith(
+        assert ' '.join(playbook_run.stdout.decode().split()).endswith(
             'localhost : ok=3 changed=1 unreachable=0 failed=0 skipped=0 rescued=0 ignored=0'
         )
+
+        assert playbook_run.returncode is 0
