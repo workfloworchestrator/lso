f130407b1e92375ea5eb5602cdbda11b6536ef09
Carolina Fernandez <carolina.fernandez@i2cat.net>
2023-04-27 09:13:23 +0200

    feature/docker-env: add Dockerfile and scripts

---
+++ docker/docker-start.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+goat_name="goat-lso"
+goat_image="goat/lso"
+goat_tag="1.0"
+
+if [[ $(docker image list | grep "${goat_image}" | grep "${goat_tag}" | wc -l) -eq 0 ]]; then
+  docker build -f docker/Dockerfile -t ${goat_image}:${goat_tag} .
+fi
+if [[ $(docker ps -a | grep "${goat_image}:${goat_tag}" | wc -l) -eq 0 ]]; then
+  docker run -d -p 44444:44444 --name ${goat_name} ${goat_image}:${goat_tag} >/dev/null 2>&1
+fi
+if [[ "$( docker container inspect -f '{{.State.Status}}' ${goat_name} )" != "running" ]]; then
+  docker start ${goat_name} >/dev/null 2>&1
+fi
+
+sleep 1
+
+# Check endpoints
+curl -f http://localhost:44444/docs >/dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+  echo "LSO is running. OpenAPI available at http://localhost:44444/docs"
+else
+  echo "LSO is not running"
+fi
