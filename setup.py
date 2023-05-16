baaeb49a66363105e4bfa2f485caf4627399b711
Karel van Klink <karel.vanklink@geant.org>
2023-05-16 10:50:55 +0200

    small compatibility updates

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",
     author='GÉANT Orchestration & Automation Team',
     author_email='TBD',
-    description='Lightweight Ansible Runner Provioner',
+    description='Lightweight Service Orchestrator',
     url='https://gitlab.geant.org/goat/gap/lso',
     packages=find_packages(),
     install_requires=[
