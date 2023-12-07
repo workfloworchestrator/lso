ccdf24b762ca64346317c8407c611eb6dfd9dab2
geant-release-service <swd@geant.org>
2023-12-07 13:45:17 +0000

    incremented version for next development iteration

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="goat-lso",
-    version="0.14",
+    version="0.15",
     author="GÉANT Orchestration & Automation Team",
     author_email="goat@geant.org",
     description="Lightweight Service Orchestrator",
