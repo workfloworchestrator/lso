6fd911285c2e0ff71c6df60ecf6ed98ce9301547
Karel van Klink <karel.vanklink@geant.org>
2024-03-06 12:35:38 +0100

    Change license from MIT to Apache 2.0

--- README.md
+++ README.md
@@ -2,12 +2,6 @@
 
 LSO: an API that allows for remotely executing Ansible playbooks
 
-
-## General documentation
-
-General documentation is available at <https://docs.gap.geant.org/>
-
-
 ## Code documentation
 
 Code documentation can be found at <https://workfloworchestrator.org/lso>
--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
+# Copyright 2023-2024 GÉANT Vereniging.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 from setuptools import find_packages, setup
 
 setup(
-    name="goat-lso",
+    name="lso",
     version="1.5",
     author="GÉANT Orchestration & Automation Team",
     author_email="goat@geant.org",
     description="Lightweight Service Orchestrator",
-    url="https://gitlab.software.geant.org/goat/gap/lso",
+    url="https://github.com/workfloworchestrator/lso",
     packages=find_packages(),
     install_requires=[
         "ansible_merge_vars~=5.0.0",
@@ -29,14 +42,14 @@ setup(
         "uvicorn[standard]~=0.22.0",
         "xmltodict~=0.13.0",
     ],
-    license="MIT",
-    license_files=("LICENSE.txt",),
+    license="Apache Software License",
+    license_files=("LICENSE",),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.11",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
-        "Development Status :: 3 - Alpha",
+        "Development Status :: 5 - Production",
         "Framework :: FastAPI",
         "Intended Audience :: System Administrators",
         "Intended Audience :: Telecommunications Industry",
