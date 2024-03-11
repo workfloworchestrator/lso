3a667e409863ae640cc5331c9fb7af6f564fb08a
Karel van Klink <karel.vanklink@geant.org>
2024-03-11 16:36:36 +0100

    Update package description

--- lso/__init__.py
+++ lso/__init__.py
@@ -11,7 +11,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Automatically invoked app factory."""
+"""LSO, an API for remotely running Ansible playbooks."""
 
 __version__ = "1.0.0"
 
